2864a6634c0c9b15806337f88fd5c4148ae3790b
Mike Boldsen <mikeboldsen2004@gmail.com>
2024-02-01 10:54:23 +0100

    fgg

--- src/components/ourVideoPlayer/ourVideoPlayer.js
+++ src/components/ourVideoPlayer/ourVideoPlayer.js
@@ -16,20 +16,27 @@ const OurVideoPlayer = ({path}) => {
     const changeVolume = (e) => {
         videoRef.current.volume = e.target.value
     };
+    const fullScreen = () => {
+        videoRef.current.requestFullscreen();
+    }
+
+  
     
 
     return (
         <div className={styles.container}>
             <div className={styles.ourVideo}>
-                    <video src={path} ref={videoRef} controls muted></video>
+                    <video src={path} id="video1" ref={videoRef} controls muted></video>
             </div>
          
             <div className={styles.ourControls}>
 
                 <div className={styles.ourCtrlBtn} onClick={playVideo}>Play</div>
                 <div className={styles.ourCtrlBtn} onClick={pauseVideo}>Pause</div>
+                <div className={styles.ourFullscreenBtn} onClick={fullScreen}>Fullscreen</div>
 
                 <input type="range" min="0" max="1" step="0.01" onChange={changeVolume} />
+                
             </div>
         </div>
     )
